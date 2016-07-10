d5a64c2d00c5ab2f120daa1141619f49673e9d3e
koziscool <kozjob@yahoo.com>
2016-07-10 18:48:03 -0500

    order by

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var widgets = angular.module('widgetsApp', []);
 widgets.controller( 'RestaurantCtrl',
   ['$scope', function($scope){
 
-    $scope.restaurants = [];
+    $scope.restaurants = [
+        {"name": "john", "foodType": "mexican"}, 
+        {"name": "julia", "foodType": "hamburgers"}, 
+        {"name": "aaaa", "foodType": "aaaaa"}, 
+        {"name": "bbbb", "foodType": "BBB"}
+    ];
 
     $scope.processForm = function() {
         var newRestaurant = {};
